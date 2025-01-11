50f2f2ac8682cdb9bc2c241d9d50a1653d928e96
Seyedmasih Tabaei <masihtabaei@gmail.com>
2025-01-11 07:31:26 +0100

    Continues the work on #19
    Added a simple headup display to display some weather data related to
    the location

---
+++ unity/Assets/Scripts/LocationManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NativeWebSocket;
+using TMPro;
+
+
+public class LocationManager : MonoBehaviour
+{
+    private WebSocket websocket;
+
+    [SerializeField]
+    private TextMeshProUGUI temperatureField;
+
+    [SerializeField]
+    private TextMeshProUGUI appearantTemperatureField;
+
+    [SerializeField]
+    private TextMeshProUGUI rainIntensityField;
+
+    [SerializeField]
+    private TextMeshProUGUI snowIntensityField;
+
+    async void Start()
+    {
+        websocket = new WebSocket("ws://localhost:8000/ws/weather");
+
+        websocket.OnOpen += () =>
+        {
+            Debug.Log("Connection opened successfully!");
+        };
+
+        websocket.OnError += (e) =>
+        {
+            Debug.Log("Error! " + e);
+        };
+
+        websocket.OnClose += (e) =>
+        {
+            Debug.Log("Connection closed successfully!");
+        };
+
+        websocket.OnMessage += (bytes) =>
+        {
+            var message = System.Text.Encoding.UTF8.GetString(bytes);
+            //Debug.Log("Message Received: " + message);
+            WeatherData currentWeatherData = JsonUtility.FromJson<WeatherData>(message);
+            temperatureField.text = "Temperature: " + currentWeatherData.temperature;
+            appearantTemperatureField.text = $"Apparent Temperature: {currentWeatherData.apparent_temperature:F2}";
+            rainIntensityField.text = "Rain Intensity: " + currentWeatherData.rain;
+            snowIntensityField.text = "Snow Intensity: " + currentWeatherData.snowfall;
+        };
+
+
+        // waiting for messages
+        await websocket.Connect();
+    }
+
+    void Update()
+    {
+        #if !UNITY_WEBGL || UNITY_EDITOR
+            websocket.DispatchMessageQueue();
+        #endif
+    }
+
+
+    private async void OnApplicationQuit()
+    {
+        await websocket.Close();
+    }
+
+}
---
+++ unity/Assets/Scripts/WeatherData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class WeatherData
+{
+    public float latitude;
+    public float longitude;
+    public float temperature;
+    public float apparent_temperature;
+    public float rain;
+    public float snowfall;
+    public float is_day;
+}
